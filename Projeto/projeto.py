c77a4eea0493aa8f685826ebb2a6b86c0c0651a3
Tiago <72828463+TiagoWebMaster@users.noreply.github.com>
2022-10-07 19:48:28 +0100

    Tirei os nossos modelos lineares

--- Projeto/projeto.py
+++ Projeto/projeto.py
@@ -10,27 +10,7 @@ X = np.load('Xtrain_Regression1.npy')
 Y = np.load('Ytrain_Regression1.npy')
 
 
-#dc - data centered
-def linear_regression_dc(X,Y):
-    X2 = np.empty(X.shape)
-    for i,column in enumerate(X.T):
-        X2.T[i] = X.T[i] - np.mean(column)
-    Y2 = Y - np.mean(Y)
-    beta_aux = X2.transpose().dot(X2)
-
-    beta_aux = np.linalg.inv( beta_aux )
-
-    beta = (beta_aux.dot(X2.transpose())).dot(Y2)
-    print(f"Sum of squared errors {mean_squared_error(Y2, X2.dot(beta))} my centered linear regression")
-
-def linear_regression(X,Y):
-    n,m=np.shape(X)
-    Xn=np.hstack((np.ones((n,1)),X))
-
-    beta = ((np.linalg.inv((Xn.T).dot(Xn))).dot(Xn.T)).dot(Y)
-    print(f"Sum of squared errors {mean_squared_error(Y, Xn.dot(beta))} my linear regression with beta0")
-
-
+#Lasso
 lasso_vector = np.arange(0.005,0.02, 0.001)
 mean_squared_error_x = np.empty(lasso_vector.shape)
 betas = np.arange(10.0*lasso_vector.shape[0])
@@ -52,14 +32,12 @@ for i,alpha in enumerate(lasso_vector):
 
     
     
-
+#Linear
 regr = linear_model.LinearRegression()
 regr.fit(X, Y)
 Y_pred = regr.predict(X)
 #print(f"Sum of squared errors {mean_squared_error(Y, Y_pred)} sklearn regression")
 
-
-linear_regression(X,Y)
 
 
 scores = cross_val_score(regr, X, Y, scoring='neg_mean_absolute_error',cv=cv, n_jobs=-1)
