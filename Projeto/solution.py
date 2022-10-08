73997dd689fbeef523f18b3113174d5cfe4ebcb1
Tiago <72828463+TiagoWebMaster@users.noreply.github.com>
2022-10-08 14:37:03 +0100

    cenas

--- Projeto/solution.py
+++ Projeto/solution.py
@@ -7,7 +7,7 @@ Y_train = np.load('Ytrain_Regression1.npy')
 X_test = np.load('Xtest_Regression1.npy')
 
 
-reg = linear_model.Lasso(0.018)
+reg = linear_model.Lasso(0.017)
 reg.fit(X_train, Y_train)
 Y_test = reg.predict(X_test)
 
