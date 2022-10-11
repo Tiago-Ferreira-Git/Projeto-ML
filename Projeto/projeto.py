5c71728438ab632ee318b2c9552b611f46509329
jg-feliz <101978553+jg-feliz@users.noreply.github.com>
2022-10-11 20:24:01 +0100

    Update projeto.py

--- Projeto/projeto.py
+++ Projeto/projeto.py
@@ -29,7 +29,7 @@ for i,alpha in enumerate(lasso_vector):
     mean_squared_error_x[i] = mean_squared_error(Y, Y_pred)
     scores = cross_val_score(reg, X, Y, scoring='neg_mean_absolute_error',cv=cv, n_jobs=-1)
     loo_score[i] = np.mean(np.absolute(scores))
-    print(f"LOO score: {np.mean(np.absolute(scores))} | MSE: {mean_squared_error_x[i]} for alpha = {alpha}")
+    #print(f"LOO score: {np.mean(np.absolute(scores))} | MSE: {mean_squared_error_x[i]} for alpha = {alpha}")
 
     
     
@@ -45,18 +45,18 @@ Y_pred = regr.predict(X)
 
 
 
-scores = cross_val_score(regr, X, Y, scoring='neg_mean_absolute_error',cv=cv, n_jobs=-1)
-print(f"LeaveOneOut score for linear regression {np.mean(np.absolute(scores))}")
-loo_score[-1] = np.mean(np.absolute(scores))
-
-to_plot = {"lasso_vector": lasso_vector,"mean_squared_error_x": mean_squared_error_x, "label": "experiment"}
-savemat("to_plot_lasso_mse.mat",to_plot)
-to_plot = {"lasso_vector": lasso_vector,"betas": betas, "label": "experiment"}
-savemat("to_plot_lasso_betas.mat",to_plot)
-
-
-to_plot = {"lasso_vector": np.append(lasso_vector,"linear"),"loo_score": loo_score, "label": "experiment"}
-savemat("to_plot_loo_score.mat",to_plot)
+# scores = cross_val_score(regr, X, Y, scoring='neg_mean_absolute_error',cv=cv, n_jobs=-1)
+# print(f"LeaveOneOut score for linear regression {np.mean(np.absolute(scores))}")
+# loo_score[-1] = np.mean(np.absolute(scores))
+# 
+# to_plot = {"lasso_vector": lasso_vector,"mean_squared_error_x": mean_squared_error_x, "label": "experiment"}
+# savemat("to_plot_lasso_mse.mat",to_plot)
+# to_plot = {"lasso_vector": lasso_vector,"betas": betas, "label": "experiment"}
+# savemat("to_plot_lasso_betas.mat",to_plot)
+# 
+# 
+# to_plot = {"lasso_vector": np.append(lasso_vector,"linear"),"loo_score": loo_score, "label": "experiment"}
+# savemat("to_plot_loo_score.mat",to_plot)
 
 
 
@@ -67,19 +67,25 @@ X = np.load('Xtrain_Regression2.npy')
 Y = np.load('Ytrain_Regression2.npy')
 X_test = np.load('Xtest_Regression2.npy')
 std = np.std(Y, dtype=np.float64)
-mean = np.mean(X, dtype=np.float64)
+mean = np.mean(Y, dtype=np.float64)
 print(mean,std)
 delete_values = list()
 
-for i,value in enumerate(Y):
-    if  mean-std < value < mean+std: #68% integral de uma gaussiana
-        continue
-    else:
-        delete_values.append(i)
-X = np.delete(X, delete_values,axis=0)
-Y = np.delete(Y, delete_values)
-reg = linear_model.Lasso(0.017)
-reg.fit(X, Y)
-Y_pred = reg.predict(X)
-Y_test = reg.predict(X_test)
-print(mean_squared_error(Y, Y_pred))
+erro_vector= np.arange(0.77,1.57, 0.05)
+
+for n,error in enumerate(erro_vector):
+    X = np.load('Xtrain_Regression2.npy')
+    Y = np.load('Ytrain_Regression2.npy')
+    delete_values = list()
+    for i,value in enumerate(Y):
+        if  mean-error*std < abs(value) < mean+error*std: #68% integral de uma gaussiana
+            continue
+        else:
+            delete_values.append(i)
+    X = np.delete(X, delete_values,axis=0)
+    Y = np.delete(Y, delete_values)
+    reg = linear_model.Lasso(0.017)
+    reg.fit(X, Y)
+    Y_pred = reg.predict(X)
+    Y_test = reg.predict(X_test)
+    print(f"MSE: {mean_squared_error(Y, Y_pred)} for +/- {error}std")
