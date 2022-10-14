9f026dd556d1315d73b3af835007da21c52b311b
TiagoWebMaster <tiagombferreira@tecnico.ulisboa.pt>
2022-10-14 20:04:38 +0100

    Cleaned Code

--- Projeto/projeto.py
+++ Projeto/projeto.py
@@ -10,29 +10,48 @@ plt.close('all')
 X = np.load('Xtrain_Regression1.npy')
 Y = np.load('Ytrain_Regression1.npy')
 
-"""
-#Lasso
-lasso_vector = np.arange(0.005,0.02, 0.001)
-mean_squared_error_x = np.empty(lasso_vector.shape)
-betas = np.arange(10.0*lasso_vector.shape[0])
-betas = betas.reshape((10, lasso_vector.shape[0]))
-betas = np.zeros_like(betas)
-loo_score = np.empty(lasso_vector.shape[0] + 1 )
-for i,alpha in enumerate(lasso_vector):
-    reg = linear_model.Lasso(alpha)
-    reg.fit(X, Y)
-    Y_pred = reg.predict(X)
-    beta = reg.coef_
-    for j,beta_i in enumerate(beta):
-        betas[j][i] = beta_i
-    
-    mean_squared_error_x[i] = mean_squared_error(Y, Y_pred)
-    scores = cross_val_score(reg, X, Y, scoring='neg_mean_absolute_error',cv=cv, n_jobs=-1)
-    loo_score[i] = np.mean(np.absolute(scores))
-    #print(f"LOO score: {np.mean(np.absolute(scores))} | MSE: {mean_squared_error_x[i]} for alpha = {alpha}")
 
-    
-    
+#Lasso
+def study_lasso(x,y,start,end,stepping):
+    lasso_vector = np.arange(start,end,stepping)
+    mean_squared_error_x = np.empty(lasso_vector.shape)
+    betas = np.arange(10.0*lasso_vector.shape[0])
+    betas = betas.reshape((10, lasso_vector.shape[0]))
+    betas = np.zeros_like(betas)
+    loo_score = np.empty(lasso_vector.shape[0] + 1)
+    for i,alpha in enumerate(lasso_vector):
+        reg = linear_model.Lasso(alpha)
+        reg.fit(x, y)
+        y_pred = reg.predict(x)
+        beta = reg.coef_
+        for j,beta_i in enumerate(beta):
+            betas[j][i] = beta_i
+        
+        mean_squared_error_x[i] = mean_squared_error(y, y_pred)
+        scores = cross_val_score(reg, X, Y, scoring='neg_mean_absolute_error',cv=cv, n_jobs=-1)
+        loo_score[i] = np.mean(np.absolute(scores))
+        #print(f"LOO score: {loo_score[i]} | MSE: {mean_squared_error_x[i]} for alpha = {alpha}")
+
+        #comparing it to linear regression
+        regr = linear_model.LinearRegression()
+        regr.fit(x, y)
+        Y_pred = regr.predict(x)
+        scores = cross_val_score(regr, X, Y, scoring='neg_mean_absolute_error',cv=cv, n_jobs=-1)
+        loo_score[-1] = np.mean(np.absolute(scores))
+
+        #Matlab Data to be plotted 
+        to_plot = {"lasso_vector": lasso_vector,"mean_squared_error_x": mean_squared_error_x, "label": "experiment"}
+        savemat("to_plot_lasso_mse.mat",to_plot)
+        to_plot = {"lasso_vector": lasso_vector,"betas": betas, "label": "experiment"}
+        savemat("to_plot_lasso_betas.mat",to_plot)
+
+
+        to_plot = {"lasso_vector": np.append(lasso_vector,"linear"),"loo_score": loo_score, "label": "experiment"}
+        savemat("to_plot_loo_score.mat",to_plot)
+                    
+#Lasso
+#study_lasso(X,Y,0.005,0.02, 0.001)  
+
 #Linear
 regr = linear_model.LinearRegression()
 regr.fit(X, Y)
@@ -40,25 +59,7 @@ Y_pred = regr.predict(X)
 print(f"Sum of squared errors {mean_squared_error(Y, Y_pred)} sklearn regression")
 
 
-#polynomial
 
-
-
-
-scores = cross_val_score(regr, X, Y, scoring='neg_mean_absolute_error',cv=cv, n_jobs=-1)
-print(f"LeaveOneOut score for linear regression {np.mean(np.absolute(scores))}")
-loo_score[-1] = np.mean(np.absolute(scores))
-
-to_plot = {"lasso_vector": lasso_vector,"mean_squared_error_x": mean_squared_error_x, "label": "experiment"}
-savemat("to_plot_lasso_mse.mat",to_plot)
-to_plot = {"lasso_vector": lasso_vector,"betas": betas, "label": "experiment"}
-savemat("to_plot_lasso_betas.mat",to_plot)
-
-
-to_plot = {"lasso_vector": np.append(lasso_vector,"linear"),"loo_score": loo_score, "label": "experiment"}
-savemat("to_plot_loo_score.mat",to_plot)
-
-"""
 
 
 
@@ -67,45 +68,26 @@ X = np.load('Xtrain_Regression2.npy')
 Y = np.load('Ytrain_Regression2.npy')
 X_test = np.load('Xtest_Regression2.npy')
 
-delete_values = list()
-
-erro_vector= np.arange(0.77,1.57, 0.01)
 
 X = np.load('Xtrain_Regression2.npy')
 Y = np.load('Ytrain_Regression2.npy')
-to_plot = {"x": X,"ly": Y, "label": "experiment"}
-savemat("view_data.mat",to_plot)
-maxerror=0
-error=[]
-for n in range(0,int(0.2*Y.shape[0])):
-    error=[]
-    maxerror=0
-    maxerrori=0
-    regr = linear_model.LinearRegression()
-    regr.fit(X, Y)
-    Y_pred = regr.predict(X)
-    Y_test = regr.predict(X_test)
-    scores = cross_val_score(regr, X, Y, scoring='neg_mean_absolute_error',cv=cv, n_jobs=-1)
-    print(f"LOO score: {np.mean(np.absolute(scores))} | MSE: {mean_squared_error(Y, Y_pred)} -> Y {Y.shape} , X {X.shape}")
-    for i,value in enumerate(Y):
-        error.append((Y[i]-Y_pred[i])**2)
-        #print(error[i])
-        if maxerror < error[i]:
-            maxerror=error[i]
-            maxerrori=i
-        else:
-            continue
-        pass
-    print(maxerror)
-    Y=np.delete(Y,maxerrori,axis=0)
-    X=np.delete(X,maxerrori,0)
-    pass
-    
-reg = linear_model.Lasso(0.0017)
-reg.fit(X, Y)
-Y_pred = reg.predict(X)
-Y_test = reg.predict(X_test)
-scores = cross_val_score(reg, X, Y, scoring='neg_mean_absolute_error',cv=cv, n_jobs=-1)
-print(f"LOO score: {np.mean(np.absolute(scores))} | MSE: {mean_squared_error(Y, Y_pred)} -> Y {Y.shape} , X {X.shape}")
 
-#27
+def remove_outliers(x, y):
+    mse=10000
+    while mse > 0.022:
+        error=[]
+        regr = linear_model.LinearRegression()
+        regr.fit(x, y)
+        y_pred = regr.predict(x)
+        scores = cross_val_score(regr, x, y, scoring='neg_mean_absolute_error',cv=cv, n_jobs=-1)
+        mse = mean_squared_error(y, y_pred)
+        print(f"LOO score: {np.mean(np.absolute(scores))} | MSE: {mse} -> Y {y.shape} , X {x.shape}")
+        for i,value in enumerate(x): error.append((y[i]-y_pred[i])**2)
+        maxerror = max(error)
+        maxerrori = error.index(maxerror)
+        y=np.delete(y,maxerrori,axis=0)
+        x=np.delete(x,maxerrori,axis=0)
+    return x,y
+X,Y = remove_outliers(X,Y)
+print(Y.shape)
+study_lasso(X,Y,0.05,0.1, 0.001)  
--- graficos.m
+++ graficos.m
@@ -1,55 +1,53 @@
 clear('all')
 close('all')
-% to_plot_mse = load('Projeto/to_plot_lasso_mse.mat');
-% figure;
-% plot(to_plot_mse.lasso_vector,to_plot_mse.mean_squared_error_x);
-% hold on;
-% xlabel('\lambda');
-% ylabel('Mean Squared Errors');
-% grid on;
-% hold off
-% 
-% to_plot_betas = load('Projeto/to_plot_lasso_betas.mat');
-% figure;
-% hold on;
-% grid on;
-% for j=1:length(to_plot_betas.betas(:,1))
-%     xlabel('\lambda');
-%     ylabel('\beta_{i}');
-%     if  -0.05 < to_plot_betas.betas(j,1) && to_plot_betas.betas(j,1) < 0.05
-%         plot(to_plot_betas.lasso_vector,to_plot_betas.betas(j,:),'DisplayName',"Beta "+ string(j-1))
-%         legend([],'Location','northeast');
-%         pause(1)
-%     end
-% end
-% hold off
-% legend show
-% 
-% 
-% 
-% to_plot_loo_score = load('Projeto/to_plot_loo_score.mat');
-% y = to_plot_loo_score.loo_score;
-% x = strings(1,16)
-% for j=1:length(to_plot_loo_score.lasso_vector(:,1))
-%     if ~strcmp(to_plot_loo_score.lasso_vector(j,:),'linear                          ')
-%         
-%         x(j) = sprintf('\\lambda = %0.3f', str2double(to_plot_loo_score.lasso_vector(j,:)));
-%     else 
-%         x(j) = to_plot_loo_score.lasso_vector(j,:);
-%     end
-%     x(j) = strtrim(x(j));
-% end
-% figure
-% bar(y)
-% ax = gca;
-% ax.XTick = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16]; 
-% ax.XTickLabels = x;
-% ax.XTickLabelRotation = 45;
-% hold on
-% grid on
-% ylabel('LOO scores')
-% hold off
+to_plot_mse = load('Projeto/to_plot_lasso_mse.mat');
+figure;
+plot(to_plot_mse.lasso_vector,to_plot_mse.mean_squared_error_x);
+hold on;
+xlabel('\lambda');
+ylabel('Mean Squared Errors');
+grid on;
+hold off
+
+to_plot_betas = load('Projeto/to_plot_lasso_betas.mat');
+figure;
+hold on;
+grid on;
+for j=1:length(to_plot_betas.betas(:,1))
+    xlabel('\lambda');
+    ylabel('\beta_{i}');
+    %0.05 no primeiro
+    if  -1 < to_plot_betas.betas(j,1) && to_plot_betas.betas(j,1) < 1
+        plot(to_plot_betas.lasso_vector,to_plot_betas.betas(j,:),'DisplayName',"Beta "+ string(j-1))
+        legend([],'Location','northeast');
+        pause(1)
+    end
+end
+hold off
+legend show
 
 
-to_plot = load('Projeto/view_data.mat');
- plot(to_plot.x,to_plot.ly,".")
+
+to_plot_loo_score = load('Projeto/to_plot_loo_score.mat');
+y = to_plot_loo_score.loo_score;
+x = strings(1,16);
+for j=1:length(to_plot_loo_score.lasso_vector(:,1))
+    if ~strcmp(to_plot_loo_score.lasso_vector(j,:),'linear                          ')
+        
+        x(j) = sprintf('\\lambda = %0.3f', str2double(to_plot_loo_score.lasso_vector(j,:)));
+    else 
+        x(j) = to_plot_loo_score.lasso_vector(j,:);
+    end
+    x(j) = strtrim(x(j));
+end
+figure
+bar(y)
+ax = gca;
+ax.XTick = [1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16]; 
+ax.XTickLabels = x;
+ax.XTickLabelRotation = 45;
+hold on
+grid on
+ylabel('LOO scores')
+hold off
+
