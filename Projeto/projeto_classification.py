d6a11791135da859f982ef3e5a3880153df9b5d5
TiagoWebMaster <tiagombferreira@tecnico.ulisboa.pt>
2022-10-22 22:57:56 +0100

    tried :c

--- Projeto/projeto_classification.py
+++ Projeto/projeto_classification.py
@@ -4,7 +4,7 @@ import matplotlib.image as mpimg
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-
+from tensorflow.keras.layers import CenterCrop,Rescaling
 
 #Bayes classification
 from sklearn.model_selection import train_test_split
@@ -17,9 +17,11 @@ from sklearn.inspection import DecisionBoundaryDisplay
 
 X = np.load('numpy_files/Xtrain_Classification1.npy')
 y = np.load('numpy_files/ytrain_Classification1.npy')
-print(y.shape)
+#print(y.shape)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=0)
-#X = X.reshape((8273,30, 30,3))
+print(X.shape)
+X_alt = X.reshape((8273,30, 30,3))
+print(X_alt.shape)
 
 #Bayes Classification
 gnb = GaussianNB()
@@ -40,3 +42,30 @@ print("Number of mislabeled points out of a total %d points : %d"
 
 
 #keras image classification
+dense = keras.layers.Dense(units=16)
+inputs = keras.Input(shape=(30, 30, 3))
+
+
+x = CenterCrop(height=30, width=30)(inputs)
+# Rescale images to [0, 1]
+x = Rescaling(scale=1.0 / 255)(x)
+
+# Apply some convolution and pooling layers
+x = layers.Dense(128, activation="relu")(x)
+x = layers.Dense(128, activation="relu")(x)
+
+# Apply global average pooling to get flat feature vectors
+x = layers.GlobalAveragePooling2D()(x)
+
+# Add a dense classifier on top
+num_classes = 10
+outputs = layers.Dense(num_classes, activation="softmax")(x)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+
+
+model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+model.fit(X_train, y_train, batch_size=32, epochs=10)
+
+predictions = model.predict(X_test)
+#print(predictions.shape)
